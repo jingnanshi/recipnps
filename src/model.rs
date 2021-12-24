a503b0b7fcde28d06186aee254966f7ca6886a58
jingnanshi <jnshi@mit.edu>
2021-12-24 17:57:31 -0500

    slight refactor and add function to calculate reprojection costs of multiple points

--- src/model.rs
+++ src/model.rs
@@ -12,8 +12,8 @@ impl Model {
     ///
     /// We assume:
     /// p_cam = R_gt p_world + t_gt
-    fn reprojection_cost(&self, world_point: na::MatrixSlice3x1<f64>,
-                         bearing_vector: na::MatrixSlice3x1<f64>) -> f64 {
+    pub fn reprojection_dist_of(&self, world_point: na::MatrixSlice3x1<f64>,
+                                bearing_vector: na::MatrixSlice3x1<f64>) -> f64 {
         // we assume:
         // p_cam = R_gt p_world + t_gt
         let reprojection = (self.rotation * world_point + self.translation).normalize();
@@ -22,6 +22,22 @@ impl Model {
         // 1 - cos(angle between the two vectors)
         // and the range will be from 0 to 2
         return 1.0 - reprojection.dot(&bearing_vector.normalize());
+    }
+
+    /// Calculate the reprojection costs of pairs of bearing vectors and world points
+    pub fn reprojection_dists_of(&self, world_points: na::MatrixSlice3xX<f64>,
+                                 bearing_vectors: na::MatrixSlice3xX<f64>) -> na::Matrix1xX<f64> {
+        let mut reprojections: na::Matrix3xX<f64> = self.rotation * world_points;
+        for (_i, mut column) in reprojections.column_iter_mut().enumerate() {
+            column += self.translation;
+        }
+
+        let cos_angles = (reprojections.transpose() * bearing_vectors).diagonal();
+        let mut dists = na::Matrix1xX::<f64>::zeros(world_points.shape().1);
+        for i in 0..dists.shape().1 {
+            dists[i] = 1.0 - cos_angles[i];
+        }
+        return dists;
     }
 }
 
@@ -44,7 +60,7 @@ mod tests {
             col += t_gt;
         }
         let mut model_to_test = Model { rotation: rotation_gt, translation: t_gt };
-        let cost = model_to_test.reprojection_cost(p_src.column(0), p_tgt.column(0));
+        let cost = model_to_test.reprojection_dist_of(p_src.column(0), p_tgt.column(0));
         assert!(cost < 1e-7f64);
     }
 }
